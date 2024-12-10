8f7acfb2da3b40a0643a2470f4377ee9093b7820
Abimael <icemask2612@gmail.com>
2024-12-10 18:49:57 -0400

    task 5

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | xargs -I{} sh -c 'echo {},$(dig +short {} | head -n 1)' > "$1.txt"
+subfinder -d "$1" -silent -nW -o "$1.txt" -oI | xargs -I{} sh -c 'echo {},$(dig +short {} | head -n 1)' > "$1.txt"
