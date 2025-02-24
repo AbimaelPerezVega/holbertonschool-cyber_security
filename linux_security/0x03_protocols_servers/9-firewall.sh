59ef87b53d353ba19d6daaf1737d81026bd39ce2
Abimael <icemask2612@gmail.com>
2025-02-24 18:23:26 -0400

    number nine

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-iptables -F && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -j DROP
+iptables -F
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -j DROP
