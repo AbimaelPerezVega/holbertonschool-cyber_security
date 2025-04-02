e3f1a2d5dda34e0242e41cef446c831d002199ba
Abimael <icemask2612@gmail.com>
2025-04-02 15:29:59 -0400

    fixing 3

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Check if a log file is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <log_file>"
-    exit 1
-fi
-
-LOG_FILE=$1
+# Set default log file if not provided
+LOG_FILE=${1:-logs.txt}
 
 # Ensure the file exists
-if [ ! -f $LOG_FILE ]; then
+if [ ! -f "$LOG_FILE" ]; then
     echo "Error: File not found!"
     exit 1
 fi
@@ -18,7 +13,7 @@ fi
 ATTACKER_IP=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
 
 # Extract User-Agent associated with the attacker
-USER_AGENT=$(grep "^$ATTACKER_IP " "$LOG_FILE" | awk -F '"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ //')
+USER_AGENT=$(grep "^$ATTACKER_IP " "$LOG_FILE" | awk -F '\"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ //')
 
 # Print the most used User-Agent
 echo "$USER_AGENT"
